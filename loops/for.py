7b3452925d0ed70daec7af013cf539f8b0a4ed7f
Cris-aian Vergara <crisaianvergara@gmail.com>
2023-05-31 15:38:47 +0800

    added for, if, in

---
+++ loops/for.py
@@ -0,0 +1,9 @@
+friends = ["Rolf", "Jen", "Bob", "Anne"]
+
+
+for friend in friends:
+    print(f"{friend} is my friend.")
+
+
+for friend in range(4):
+    print(f"{friends[friend]} is my friend.")
